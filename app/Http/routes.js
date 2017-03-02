4ba6b11ca76c105b12fb1d6fbbf56262d1625f11
indr <mail@indr.ch>
2017-03-02 08:25:56 +0700

    Add hackcss and basic layout, style

--- app/Http/routes.js
+++ app/Http/routes.js
@@ -1,20 +1,26 @@
 'use strict'
 
 /*
-|--------------------------------------------------------------------------
-| Router
-|--------------------------------------------------------------------------
-|
-| AdonisJs Router helps you in defining urls and their actions. It supports
-| all major HTTP conventions to keep your routes file descriptive and
-| clean.
-|
-| @example
-| Route.get('/user', 'UserController.index')
-| Route.post('/user', 'UserController.store')
-| Route.resource('user', 'UserController')
-*/
+ |--------------------------------------------------------------------------
+ | Router
+ |--------------------------------------------------------------------------
+ |
+ | AdonisJs Router helps you in defining urls and their actions. It supports
+ | all major HTTP conventions to keep your routes file descriptive and
+ | clean.
+ |
+ | @example
+ | Route.get('/user', 'UserController.index')
+ | Route.post('/user', 'UserController.store')
+ | Route.resource('user', 'UserController')
+ */
 
 const Route = use('Route')
 
 Route.on('/').render('welcome')
+Route.on('/about').render('about')
+Route.on('/contact').render('contact')
+Route.on('/login').render('login')
+Route.on('/logout').render('logout')
+Route.on('/signup').render('signup')
+
